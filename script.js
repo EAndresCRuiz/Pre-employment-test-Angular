792511e7899430e5c119568d8bc0485f061f63e6
Andres <ingenieria1@besttechnologiessa.com>
2017-08-29 14:16:37 -0500

    Creation of directive 'list' to use template patient-template.html, it was done aditional requirements of test

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 angular.module('AngularTest', [])
-.controller('Controller', ['$scope', function($scope) {
+.controller('Controller', ['$scope','$http', function($scope, $http) {
 
-	$scope.age = 0;
+	$scope.query = 0;
+	$scope.patients = [];
 
+  //load data from file "patients.json"
+	$http.get('patients.json').then(function(resp){
+
+          $scope.patients = resp.data.patients;
+
+  });
+
+  //function to do filter of greater than, it receive field name to filter (prop) and a value to compare (val)
+  $scope.greaterThan = function(prop, val){
+    return function(patient){
+      return patient[prop] > val;
+    }
+  }
+
+}])
+.directive('list', [function() {
+  return {
+
+  	restrict: 'AEC',
+    templateUrl: 'patient-template.html'
+
+  };
 }]);
